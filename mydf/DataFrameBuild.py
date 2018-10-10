e2084207bc61e4fe5a2b20615836ceb3028819ef
Dale R. Merz Jr <dale.merz.contractor@8451.com>
2018-10-10 12:47:20 -0400

    finished the additon of the multidimensional dataframe generator, at least it's first generation

--- mydf/DataFrameBuild.py
+++ mydf/DataFrameBuild.py
@@ -37,44 +37,6 @@ class myDF():
     def profile_partitions(self):
         pass
 
-    def get_StructField(self,ctype):
-
-        if re.search('str',ctype):
-            field = StructField(ctype,StringType(),True)
-
-        if re.search('int',ctype):
-            field = StructField(ctype,IntegerType(),True)
-
-        if re.search('double',ctype):
-            field = StructField(ctype,DoubleType(),True)
-
-        return field
-
-    def get_string_array(self,w=8):
-        '''
-        Get an array of strings.
-        :param w: the width of the string generated.
-        '''
-        # print("need a string of %d characters" % w)
-        str1 = ''.join(random.choices(string.ascii_lowercase,k=w))
-        return str1
-
-    def get_integer_array(self,w=4,low=1000,high=9999):
-        '''
-        Get an array of strings.
-        '''
-        # print("need %d integers" % w)
-        num = random.randint(low,high)
-        return num
-
-
-    def get_double_array(self,x=0.0,y=1.0):
-        '''
-        Get an array of strings.
-        '''
-        # print("need %d doubles" % n)
-        num = random.random()
-        return num
 
 
     def df_simple_array(self,*args):
@@ -142,26 +104,82 @@ class myDF():
         return self.df
 
 
-    def decision_array_type(self,ctype,n):
+    def get_StructField(self,ctype):
+
+        if re.search('str',ctype):
+            field = StructField(ctype,StringType(),True)
+
+        if re.search('int',ctype):
+            field = StructField(ctype,IntegerType(),True)
+
+        if re.search('double',ctype):
+            field = StructField(ctype,DoubleType(),True)
+
+        return field
+
+    def get_string_array(self,w=8):
+        '''
+        Get an array of strings.
+        :param w: the width of the string generated.
+        '''
+        # print("need a string of %d characters" % w)
+        str1 = ''.join(random.choices(string.ascii_lowercase,k=w))
+
+        # print(len(str1),w)
+        return str1
+
+    def get_integer_array(self,nrange):
+        '''
+        Get an array of integers
+        '''
+        # print("need %d integers" % w)
+        num = random.randint(nrange[0],nrange[1])
+        return num
+
+
+    def get_double_array(self,nrange):
+        '''
+        Get an array of doubles
+        '''
+        # print("need %d doubles" % n)
+        # num = random.random() # 0.0 - 1.0
+        num = random.uniform(nrange[0],nrange[1])
+        return num
+
+    def decision_array_type(self,tup_ctype):
         '''
         Get an array as strings, integers, or doubles.
+        :param tup_ctype:
+        ('string1',6)               => tcxivu
+        ('string2',13)              => rlftefdzayehr
+        ('integer',(1000,9999))     => 4285
+        ('double1',(0.0,1.0))       => 0.6514825745809212
+        ('double2',(0.0,10000000))  => 2142944.0838629534
+        ('integer2',(1,10000))      => 3081
         '''
         # print("Getting a single array of type:",ctype," with %d rows." % n)
 
+        ctype = tup_ctype[0]
+        width = tup_ctype[1]
+
         if re.search('str',ctype):
-            arr = self.get_string_array(n)
+            arr = self.get_string_array(width)
+            # str1 = ''.join(random.choices(string.ascii_lowercase,k=w))
             return arr
 
         if re.search('int',ctype):
-            arr = self.get_integer_array(n)
+            arr = self.get_integer_array(width)
+            # num = random.randint(nrange[0],nrange[1])
             return arr
 
         if re.search('double',ctype):
-            arr = self.get_double_array(n)
+            nrange = width
+            arr = self.get_double_array(nrange)
+            # num = random.uniform(nrange[0],nrange[1])
             return arr
 
 
-    def df_multidimensional(self,r,c,column_types,*kwargs):
+    def df_multidimensional(self,r,c,column_types):
         '''
         :param r:
         :param c:
@@ -175,29 +193,24 @@ class myDF():
         lst_fields = []
 
         for ctype in column_types:
-            # print(ctype)
-            field = self.get_StructField(ctype)
 
+            field = self.get_StructField(ctype[0])
             lst_arr = []
 
             for i in range(r):
-                entry = self.decision_array_type(ctype,r)
 
-                # print(entry)
+                entry = self.decision_array_type(ctype)
                 lst_arr.append(entry)
 
-            # print(field)
             lst_fields.append(field)
             lst_total.append(lst_arr)
 
-
         # print(lst_fields)
         # print(lst_total)
-
         lst_zipped = list(zip(*lst_total))
         # print(lst_zipped)
 
-        pdx = pd.DataFrame(lst_zipped,columns=column_types)
+        pdx = pd.DataFrame(lst_zipped,columns=[c[0] for c in column_types])
         self.df = self.spark.createDataFrame(pdx)
         return self.df
 
