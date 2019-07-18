b8df5618ece9fe5f58f5c400ba5a08712ab458c0
Dale R. Merz Jr <merz.drm@gmail.com>
2019-07-18 11:07:51 -0400

    added basic dataframe example

--- notebooks/convert.py
+++ notebooks/convert.py
@@ -3,10 +3,6 @@ import os
 import sys
 import re
 from glob import glob
-print(sys.version)
-
-# usage:
-# ./convert.py -d incomplete/
 
 # mylib:
 my_library = os.path.expanduser('~/.myconfigs')
@@ -22,6 +18,10 @@ sys.path.append(my_library)
 # from mylib.regex import reg_ex
 # from mylib.myargs import parse_arguments
 from mylib.run_command import run_command
+
+
+# usage:
+# ./convert.py -d incomplete/
 
 #  ---------------------------------------------------------  #
 #  argparse                                                   #
@@ -43,54 +43,67 @@ def parse_arguments():
     parser.add_argument("-f","--nfile",help="notebook file")
     args = vars(parser.parse_args())
     return args
-args = parse_arguments()
 
-my_dir = os.path.dirname(os.path.abspath('__file__'))
+def main():
+    print(sys.version)
 
+    # mylib:
+    my_library = os.path.expanduser('~/.myconfigs')
+    sys.path.append(my_library)
 
+    args = parse_arguments()
 
-# if 'subdir' in args:
-try:
-    dirname = args['subdir']
-    fp_dirname = os.path.join(my_dir,dirname)
-    lst_files = glob(os.path.join(fp_dirname,'*.ipynb'))
-except:
+    my_dir = os.path.dirname(os.path.abspath('__file__'))
+
+    if 'outdir' not in args:
+        outdir = my_dir
+    else:
+        outdir = "."
+    fp_outdir = os.path.join(my_dir, outdir)
+
+    # if 'subdir' in args:
     try:
-        lst_files = [os.path.join(my_dir,args['nfile'])]
+        dirname = args['subdir']
+        fp_dirname = os.path.join(my_dir,dirname)
+        lst_files = glob(os.path.join(fp_dirname, '*.ipynb'))
     except:
-        pass
-
-# if 'nfile' in args:
-# try:
-
-outdir = args['outdir']
-fp_outdir = os.path.join(my_dir,outdir)
-
-# print("my_dir:",my_dir)
-# print("in-dir:",fp_dirname)
-# print("out-dir:",fp_outdir)
-# sys.exit()
+        try:
+            lst_files = [os.path.join(my_dir,args['nfile'])]
+        except:
+            lst_files = []
+            # pass
 
 
-# print(lst_files)
 
-for nb_file in lst_files:
-    # print(nb_file)
-    run_command(['jupyter','nbconvert',nb_file])
-    html_file = re.sub('ipynb','html',nb_file)
-    md_filename = re.sub('html','md',os.path.basename(html_file))
-    dest_file = os.path.join(fp_outdir,md_filename)
+    # print("my_dir:",my_dir)
+    # print("in-dir:",fp_dirname)
+    # print("out-dir:",fp_outdir)
+    # sys.exit()
+    # print(lst_files)
 
-    print(nb_file)
-    print(html_file)
-    print(dest_file)
+    for nb_file in lst_files:
+        nb_file = os.path.normpath(nb_file)
+        print(nb_file)
+        # break
+        # run_command(['jupyter','nbconvert',nb_file])
+        run_command(['jupyter-nbconvert.exe', nb_file])
+        html_file = re.sub('ipynb', 'html', nb_file)
+        md_filename = re.sub('html', 'md', os.path.basename(html_file))
+        dest_file = os.path.join(fp_outdir, md_filename)
 
-    with open(html_file) as fp:
-        text = fp.read()
+        print(nb_file)
+        print(html_file)
+        print(dest_file)
 
-    os.remove(html_file)
+        with open(html_file) as fp:
+            text = fp.read()
 
-    with open(dest_file,"w") as fpo:
-        fpo.write(text)
+        os.remove(html_file)
 
-    # break
+        with open(dest_file,"w") as fpo:
+            fpo.write(text)
+
+        # break
+
+if __name__ == '__main__':
+    main()
